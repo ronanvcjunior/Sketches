323e613ea3351489ecc2d30dff9d077cf8b081d4
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2021-06-26 21:42:44 -0300

    double-range-slider

--- js/double-range-slider.js
+++ js/double-range-slider.js
@@ -1,32 +1,25 @@
 window.onload = function(){
-    slideOne();
-    slideTwo();
+    fillColor()
 }
-
 let sliderOne = document.getElementById("slider-1");
 let sliderTwo = document.getElementById("slider-2");
 let displayValOne = document.getElementById("range1");
 let displayValTwo = document.getElementById("range2");
-let minGap = 5;
 let sliderTrack = document.querySelector(".slider-track");
 let sliderMaxValue = document.getElementById("slider-1").max;
 
-function slideOne(){
-    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
-        sliderOne.value = parseInt(sliderTwo.value) - minGap;
-    }
-    displayValOne.textContent = sliderOne.value;
-    fillColor();
-}
-function slideTwo(){
-    if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap){
-        sliderTwo.value = parseInt(sliderOne.value) + minGap;
-    }
-    displayValTwo.textContent = sliderTwo.value;
-    fillColor();
-}
 function fillColor(){
-    percent1 = (sliderOne.value / sliderMaxValue) * 100;
-    percent2 = (sliderTwo.value / sliderMaxValue) * 100;
-    sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #3264fe ${percent1}% , #3264fe ${percent2}%, #dadae5 ${percent2}%)`;
+    percent1 = Math.round(parseFloat((sliderOne.value / sliderMaxValue) * 100));
+    percent2 = Math.round(parseFloat((sliderTwo.value / sliderMaxValue) * 100));
+
+    if(percent1 > percent2){
+        let mudança = percent1
+        percent1 = percent2
+        percent2 = mudança
+    }
+
+    displayValOne.textContent = (percent1)
+    displayValTwo.textContent = (percent2)
+    
+    sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}% , #3264fe ${percent1}% , #3264fe ${percent2}%, #dadae5 ${percent2}%)`
 }
